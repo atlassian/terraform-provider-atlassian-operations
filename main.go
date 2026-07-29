3d89bc012ecbdad99f460e27e7513b2055356847
Robert Slotte <slotte.robert@gmail.com>
2026-07-29 10:58:06 +0100

    Configure CMC provider releases

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary.
 	version = "dev"
+	address = "registry.terraform.io/cmclabs/atlassian-operations"
 
 	// goreleaser can pass other information to the main package, such as the specific commit
 	// https://goreleaser.com/cookbooks/using-main.version/
@@ -29,10 +30,7 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/atlassian/atlassian-operations",
+		Address: address,
 		Debug:   debug,
 	}
 
